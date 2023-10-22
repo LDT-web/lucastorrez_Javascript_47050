a191369d348283bd7f98574f27755304e468ea74
Lucas Torrez <ldtorrez02@gmail.com>
2023-10-22 12:29:05 -0300

    entrega final

--- js/main.js
+++ js/main.js
@@ -1,176 +1,23 @@
-// PRODUCTOS
-const productos = [
-  // Zapatillas
-  {
-    id: "zapatilla-01",
-    titulo: "zapatilla 01",
-    imagen: "./img/zapatillas/01.jpg",
-    categoria: {
-      nombre: "zapatillas",
-      id: "zapatillas",
-    },
-    precio: 1000,
-  },
-  {
-    id: "zapatilla-02",
-    titulo: "zapatilla 02",
-    imagen: "./img/zapatillas/02.jpg",
-    categoria: {
-      nombre: "zapatillas",
-      id: "zapatillas",
-    },
-    precio: 1000,
-  },
-  {
-    id: "zapatilla-03",
-    titulo: "zapatilla 03",
-    imagen: "./img/zapatillas/03.jpg",
-    categoria: {
-      nombre: "zapatillas",
-      id: "zapatillas",
-    },
-    precio: 1000,
-  },
-  {
-    id: "zapatilla-04",
-    titulo: "zapatilla 04",
-    imagen: "./img/zapatillas/04.jpg",
-    categoria: {
-      nombre: "zapatillas",
-      id: "zapatillas",
-    },
-    precio: 1000,
-  },
-  // remeras
-  {
-    id: "remera-01",
-    titulo: "remera 01",
-    imagen: "./img/remeras/01.jpg",
-    categoria: {
-      nombre: "remeras",
-      id: "remeras",
-    },
-    precio: 1000,
-  },
-  {
-    id: "remera-02",
-    titulo: "remera 02",
-    imagen: "./img/remeras/02.jpg",
-    categoria: {
-      nombre: "remeras",
-      id: "remeras",
-    },
-    precio: 1000,
-  },
-  {
-    id: "remeras-03",
-    titulo: "remeras 03",
-    imagen: "./img/remeras/03.jpg",
-    categoria: {
-      nombre: "remeras",
-      id: "remeras",
-    },
-    precio: 1000,
-  },
-  {
-    id: "remera-04",
-    titulo: "remera 04",
-    imagen: "./img/remeras/04.jpg",
-    categoria: {
-      nombre: "remeras",
-      id: "remeras",
-    },
-    precio: 1000,
-  },
-  {
-    id: "remera-05",
-    titulo: "remera 05",
-    imagen: "./img/remeras/05.jpg",
-    categoria: {
-      nombre: "remeras",
-      id: "remeras",
-    },
-    precio: 1000,
-  },
-  {
-    id: "remera-06",
-    titulo: "remera 06",
-    imagen: "./img/remeras/06.jpg",
-    categoria: {
-      nombre: "remeras",
-      id: "remeras",
-    },
-    precio: 1000,
-  },
-  {
-    id: "remera-07",
-    titulo: "remera 07",
-    imagen: "./img/remeras/07.jpg",
-    categoria: {
-      nombre: "remeras",
-      id: "remeras",
-    },
-    precio: 1000,
-  },
+let productos = [];
 
-  // Pantalones
-  {
-    id: "pantalon-01",
-    titulo: "Pantalón 01",
-    imagen: "./img/pantalones/01.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
-    },
-    precio: 1000,
-  },
-  {
-    id: "pantalon-02",
-    titulo: "Pantalón 02",
-    imagen: "./img/pantalones/02.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
-    },
-    precio: 1000,
-  },
-  {
-    id: "pantalon-03",
-    titulo: "Pantalón 03",
-    imagen: "./img/pantalones/03.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
-    },
-    precio: 1000,
-  },
-  {
-    id: "pantalon-04",
-    titulo: "Pantalón 04",
-    imagen: "./img/pantalones/04.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
-    },
-    precio: 1000,
-  },
-  {
-    id: "pantalon-05",
-    titulo: "Pantalón 05",
-    imagen: "./img/pantalones/05.jpg",
-    categoria: {
-      nombre: "Pantalones",
-      id: "pantalones",
-    },
-    precio: 1000,
-  },
-];
+fetch("./js/productos.json")
+  .then((response) => response.json())
+  .then((data) => {
+    productos = data;
+    cargarProductos(productos);
+  });
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
 const numerito = document.querySelector("#numerito");
+
+botonesCategorias.forEach((boton) =>
+  boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+  })
+);
 
 function cargarProductos(productosElegidos) {
   contenedorProductos.innerHTML = "";
@@ -236,6 +83,26 @@ if (productosEnCarritoLS) {
 }
 
 function agregarAlCarrito(e) {
+  Toastify({
+    text: "Producto agregado",
+    duration: 3000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #4b33a8, #785ce9)",
+      borderRadius: "2rem",
+      textTransform: "uppercase",
+      fontSize: ".75rem",
+    },
+    offset: {
+      x: "1.5rem", // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+      y: "1.5rem", // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+
   const idBoton = e.currentTarget.id;
   const productoAgregado = productos.find(
     (producto) => producto.id === idBoton
